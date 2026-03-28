dc6cc62e76caea44525de2412be82ca30dd777ea
MohamedAshrafzZ <mashrafzz957@gmail.com>
2026-03-28 14:40:56 +0200

    Question 2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Assignmentadv
+﻿using System.ComponentModel;
+
+namespace Assignmentadv
 {
     internal class Program
     {
@@ -12,6 +14,22 @@
             //It allows you to create a class that can be used with different types of data without having to write separate code for each type.
             //Generics provide type safety, code reusability, and improved performance by allowing you to define classes, methods,
             //and data structures that can operate on any data type while still maintaining type safety at compile time.
+
+            #endregion
+
+            #region Question 2 
+            //Q2: Write a generic class Container<T> with Add and Get methods.
+
+            //Answer :
+
+            //Container<string> stringContainer = new Container<string>();
+
+            //stringContainer.Add("Hello, World!");
+            //Console.WriteLine(stringContainer.Get());
+
+            //Container<int> intContainer = new Container<int>();
+            //intContainer.Add(3);
+            //Console.WriteLine(intContainer.Get());
 
             #endregion
         }
